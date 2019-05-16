849529ba472f765f167c2565a6ed581c2449bde3
ThePriest <gnstoqnov@yahoo.com>
2019-05-16 11:14:01 +0300

    adding comments

--- src/com/galin/Main.java
+++ src/com/galin/Main.java
@@ -3,34 +3,34 @@ import java.util.*;
 
 
 class Student{       // Клас за съсздаване на обекти от тип студент
-    private String name;   // Поле за запомняне на име
+    private String name;     // Поле за запомняне на име
     private int[] marks = new int[5];      // Поле за запомняне на 5 оценки (масив)
 
     public String getName(){      // Метод, който връща стпъността на полето  Name
         return name;
-    }
+    }       // Getter за свойството име. Връща стойността
 
     public void setName(String name){       // Метод за назначаване на полето име
         this.name = name;
-    }
+    }       // Setter на свойството име, задаваме стойноост на име
 
     public int[] getMarks(){        // Метод който връща масива с оценките
         return marks;
-    }
+    }        // Връща масива с оценките
 
     public void setMarks(int[] marks) {        // Метод, който назначава масива с оценките
         this.marks = marks;
-    }
+    }        // Задаваме стойността на масива
 }
 
 public class Main {       // Основен клас
 
     public static void main(String[] args) {      // Главен метод, входна точка на прилижението
 
-        Student[] students = new Student[10];      // Създаваме масив от 10 студента
-        inputdata(students);
-        sortByMarks(students);
-        outputData(students);
+        Student[] students = new Student[10];      // Създаваме масив от 10 студента. Масивът е празен. Масивът е референтен тип.
+        inputdata(students);      // Попълване на масива със студенти
+        sortByMarks(students);        // Сортиране на масива по среден успех
+        outputData(students);       // Извеждане елементите на масива на конзолата
 
     }
 
@@ -55,11 +55,12 @@ public class Main {       // Основен клас
         }
     }
 
-    static void sortByMarks(Student[] students){
-        Student swap;
-        for (int i = 0; i < students.length; i++){
-           for (int j = i + 1; j < students.length; j++){
-               if (calculateaveragemarks(students[i].getMarks()) > calculateaveragemarks(students[j].getMarks())){
+    static void sortByMarks(Student[] students){       // Метод за сортиране на масива студенти по средно аритметично по метода на мехурчето
+        Student swap;        // Потготвяме допълнителна променлива за размяна позициите на студентите
+        for (int i = 0; i < students.length; i++){         // Преминаваме през всеки студент в масива
+           for (int j = i + 1; j < students.length; j++){         // Сравняваме студента от външния цикъл със всички следващи
+               if (calculateaveragemarks(students[i].getMarks()) < calculateaveragemarks(students[j].getMarks())){         // Ако средното аритметично е по - малко им разменяме местата им, за да се подредят в низходящ ред
+                   // Разменяме местата
                    swap = students[i];
                    students[i] = students[j];
                    students[j] = swap;
@@ -71,18 +72,23 @@ public class Main {       // Основен клас
 
     static double calculateaveragemarks(int[] marks){ // Метод за изчисляване на средно аритметично
 
-        int sum = 0;
-
+        int sum = 0;     //Подготвяме променлива за натрупване на сумата от оценките
+        // Сумираме всички оценки
         for (int i = 0; i < marks.length; i++){
             sum += marks[i];
         }
-        return sum / 5.0;
+        return sum / 5.0;      // Намираме и връщаме средното аритметично
     }
 
-    static void outputData(Student[] students){
-        for (int i = 0; i < students.length; i++){
-            System.out.println(students[i].getName());
-            System.out.println(calculateaveragemarks(students[i].getMarks()));
+    static void outputData(Student[] students){        // Метод за отпечатване елементите на масива със студенти
+        for (int i = 0; i < students.length; i++){        // Минаваме през всеки от елементите на масива
+            System.out.printf("%s. ", i);          // Първо отпечатваме поредния номер
+            System.out.print(students[i].getName() + " ");       // Отпечатваме името на студента
+            for (int j = 0; j < students[i].getMarks().length; j++){        // Минаваме през всички оценки на студента
+                System.out.print(students[i].getMarks()[j] + ", ");         // Отпечатваме всяка оценка
+            }
+            System.out.print(calculateaveragemarks(students[i].getMarks()));         // Отпечатваме средното аритметично на оценките
+            System.out.println();         // Минаваме на нов ред
         }
     }
 }
